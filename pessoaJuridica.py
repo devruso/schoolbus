7413aca902fd46d2e38d5abfa16368f3257c3429
devruso <jamilsonjr@live.com>
2023-11-29 21:49:56 -0300

    feat: Add pessoa jurídica

---
+++ pessoaJuridica.py
@@ -0,0 +1,25 @@
+class PessoaJuridica(Pessoa):
+    def __init__(self,nome_oficial, cpf, endereco, telefone, nome_fantasia, num_funcionarios):
+        super().__init__(nome_oficial, cpf, endereco, telefone)
+        self.nome_fantasia = nome_fantasia
+        self.num_funcionarios = num_funcionarios
+    
+    def exibirDados(self):
+        return super().exibirDados() + f"Nome Fantasia: {self.nome_fantasia}\n Número de Funcionários: {self.num_funcionarios}\n"
+
+    def verificarTipo(self):
+        return "Pessoa Jurídica"
+    
+    @nome_fantasia.getter
+    def nome_fantasia(self):
+        return self.nome_fantasia
+    @nome_fantasia.setter
+    def nome_fantasia(self, nome_fantasia):
+        self.nome_fantasia = nome_fantasia
+    
+    @num_funcionarios.getter
+    def num_funcionarios(self):
+        return self.num_funcionarios
+    @num_funcionarios.setter
+    def num_funcionarios(self, num_funcionarios):
+        self.num_funcionarios = num_funcionarios
