d12bb8c48b36fc168627872372a545014dd06d3d
devruso <jamilsonjr@live.com>
2023-12-05 19:57:52 -0300

    fix: Corrige import, atributos e getters

--- pessoaJuridica.py
+++ pessoaJuridica.py
@@ -1,25 +1,26 @@
+from pessoa import Pessoa
 class PessoaJuridica(Pessoa):
     def __init__(self,nome_oficial, cpf_cnpj, endereco, telefone, nome_fantasia, num_funcionarios):
         super().__init__(nome_oficial, cpf_cnpj, endereco, telefone)
-        self.nome_fantasia = nome_fantasia
-        self.num_funcionarios = num_funcionarios
+        self._nome_fantasia = nome_fantasia
+        self._num_funcionarios = num_funcionarios
     
     def exibirDados(self):
-        return super().exibirDados() + f"Nome Fantasia: {self.nome_fantasia}\n Número de Funcionários: {self.num_funcionarios}\n"
+        return super().exibirDados() + f"Nome Fantasia: {self._nome_fantasia}\nNúmero de Funcionários: {self._num_funcionarios}\n"
 
     def verificarTipo(self):
         return "Pessoa Jurídica"
     
-    @nome_fantasia.getter
+    @property
     def nome_fantasia(self):
-        return self.nome_fantasia
+        return self._nome_fantasia
     @nome_fantasia.setter
     def nome_fantasia(self, nome_fantasia):
-        self.nome_fantasia = nome_fantasia
+        self._nome_fantasia = nome_fantasia
     
-    @num_funcionarios.getter
+    @property
     def num_funcionarios(self):
-        return self.num_funcionarios
+        return self._num_funcionarios
     @num_funcionarios.setter
     def num_funcionarios(self, num_funcionarios):
-        self.num_funcionarios = num_funcionarios
+        self._num_funcionarios = num_funcionarios
