e1561d93026b10db9e9b4e35707e66649439fa4d
devruso <jamilsonjr@live.com>
2023-12-05 19:55:44 -0300

    fix: Corrige atributos, import  e metodos

--- motorista.py
+++ motorista.py
@@ -1,36 +1,41 @@
+from pessoaFisica import PessoaFisica
 class Motorista(PessoaFisica):
-    def __init__(self, nome_oficial, cpf_cnpj, data_nascimento, num_habilitacao, terceirizado, num_contrato):
-        super().__init__(nome_oficial, cpf_cnpj, data_nascimento)
-        self.num_habilitacao = num_habilitacao
-        self.terceirizado = terceirizado
-        self.num_contrato = num_contrato
+    def __init__(self, nome_oficial, cpf_cnpj, data_nascimento,endereco,telefone,nome,mae,pai,naturalidade, num_habilitacao,categorias, terceirizado, num_contrato):
+        super().__init__(nome_oficial, cpf_cnpj, data_nascimento,endereco,telefone,nome,mae,pai,naturalidade)
+        self._num_habilitacao = num_habilitacao
+        self._terceirizado = terceirizado
+        self._num_contrato = num_contrato
+        self._categorias = []
     
     def exibirDados(self):
-        return super().exibirDados() + f"Número da Habilitação: {self.num_habilitacao}\n Terceirizado: {self.terceirizado}\n Número do Contrato: {self.num_contrato}\n"
+        return super().exibirDados() + f"Número da Habilitação: {self._num_habilitacao}\n Terceirizado: {self._terceirizado}\n Número do Contrato: {self._num_contrato}\n"
     
     def verificarTipo():
         return "Motorista"
     
-    @terceirizado.getter
+    @property
     def terceirizado(self):
-        return self.terceirizado
+        return self._terceirizado
     @terceirizado.setter
     def terceirizado(self, terceirizado):
-        self.terceirizado = terceirizado
+        self._terceirizado = terceirizado
 
-    @num_contrato.getter
+    @property
     def num_contrato(self):
-        return self.num_contrato
+        return self._num_contrato
     @num_contrato.setter
     def num_contrato(self, num_contrato):
-        self.num_contrato = num_contrato
+        self._num_contrato = num_contrato
 
-    @num_habilitacao.getter
+    @property
     def num_habilitacao(self):
-        return self.num_habilitacao
+        return self._num_habilitacao
     @num_habilitacao.setter
     def num_habilitacao(self, num_habilitacao):
-        self.num_habilitacao = num_habilitacao
+        self._num_habilitacao = num_habilitacao
+    
+    def adicionarCategoria(self, categoria):
+        self._categorias.append(categoria)
     
 
     
