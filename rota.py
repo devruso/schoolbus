e7d9667e67350dd733faf28d41b49992776827d0
devruso <jamilsonjr@live.com>
2023-12-05 19:59:17 -0300

    fix: Corrige atributos e adiciona método

--- rota.py
+++ rota.py
@@ -1,17 +1,22 @@
 class Rota:
-    rotasCriadas = 0
-    id = 0
+    _rotasCriadas = 0
+    _id = 0
     def __init__(self, id, pontosDeParada):
-        self.id = Rota.id
-        self.pontosDeParada = pontosDeParada
-        Rota.rotasCriadas += 1
-        Rota.id += 1
-    
+        self._id = Rota._id
+        self._pontosDeParada = pontosDeParada
+        Rota._rotasCriadas += 1
+        Rota._id += 1
+        
+    @property
     def rotasCriadas(self):
-        return Rota.rotasCriadas
+        return Rota._rotasCriadas
     
     def demandaTotal(self):
         demanda = 0
-        for ponto in self.pontosDeParada:
-            demanda += ponto.demanda
+        for ponto in self._pontosDeParada:
+            demanda += 1
         return demanda
+    
+    def adicionarPontoDeParada(self, pontoDeParada):
+        self._pontosDeParada.append(pontoDeParada)
+    
