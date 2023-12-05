7925f87d5bf569a7429d5aa307d3bc597931ce3a
devruso <jamilsonjr@live.com>
2023-12-05 19:53:13 -0300

    fix: Corrige atributos e getter

--- contrato.py
+++ contrato.py
@@ -1,12 +1,12 @@
 class Contrato:
-    def __init__(self, num_contrato, data_inicio, data_fim, valor, motoristas, veiculos, fornecedor):
-        self.num_contrato = num_contrato
-        self.data_inicio = data_inicio
-        self.data_fim = data_fim
-        self.valor = valor
-        self.motoristas = motoristas
-        self.veiculos = veiculos
-        self.fornecedor = fornecedor
+    def __init__(self, num_contrato, data_inicio, data_fim, valor, fornecedor):
+        self._num_contrato = num_contrato
+        self._data_inicio = data_inicio
+        self._data_fim = data_fim
+        self._valor = valor
+        self._fornecedor = fornecedor
+        self._motoristas = []
+        self._veiculos = []
 
     def exibirDados(self):
         return f"Número do Contrato: {self.num_contrato}\n Data de Início: {self.data_inicio}\n Data de Fim: {self.data_fim}\n Valor: {self.valor}\n Motoristas: {self.motoristas}\n Veículos: {self.veiculos}\n Fornecedor: {self.fornecedor}\n"
@@ -14,65 +14,68 @@ class Contrato:
     def isTerceirizado(self):
         return self.motorista.isTerceirizado()
     
+    @property
+    def num_contrato(self):
+        return self._num_contrato
     @num_contrato.setter
     def num_contrato(self, num_contrato):
-        self.num_contrato = num_contrato
-    @num_contrato.getter
-    def num_contrato(self):
-        return self.num_contrato
+        self._num_contrato = num_contrato
 
+    @property
+    def data_inicio(self):
+        return self._data_inicio
     @data_inicio.setter
     def data_inicio(self, data_inicio):
-        self.data_inicio = data_inicio
-    @data_inicio.getter
-    def data_inicio(self):
-        return self.data_inicio
+        self._data_inicio = data_inicio
 
+    @property
+    def data_fim(self):
+        return self._data_fim
     @data_fim.setter
     def data_fim(self, data_fim):
-        self.data_fim = data_fim
-    @data_fim.getter
-    def data_fim(self):
-        return self.data_fim
+        self._data_fim = data_fim
 
+    @property
+    def valor(self):
+        return self._valor
     @valor.setter
     def valor(self, valor):
-        self.valor = valor
-    @valor.getter
-    def valor(self):
-        return self.valor
+        self._valor = valor
     
+    @property
+    def tipo_contrato(self):
+        return self._tipo_contrato
     @tipo_contrato.setter
     def tipo_contrato(self, tipo_contrato):
-        self.tipo_contrato = tipo_contrato
+        self._tipo_contrato = tipo_contrato
 
+    @property
+    def motoristas(self):
+        return self._motoristas
     @motoristas.setter
     def motoristas(self, motoristas):
-        self.motoristas = motoristas
-    @motoristas.getter
-    def motoristas(self):
-        return self.motoristas
+        self._motoristas = motoristas
     
+    @property
+    def fornecedor(self):
+        return self._fornecedor
     @fornecedor.setter
     def fornecedor(self, fornecedor):
-        self.fornecedor = fornecedor
-    @fornecedor.getter
-    def fornecedor(self):
-        return self.fornecedor
+        self._fornecedor = fornecedor
     
     def adicionarMotorista(self, motorista):
-        if motorista not in self.motoristas:
-            self.motoristas.append(motorista)
+        if motorista not in self._motoristas:
+            self._motoristas.append(motorista)
         else:
             print("Motorista já cadastrado")
     def removerMotorista(self, motorista):
-        self.motoristas.remove(motorista)
+        self._motoristas.remove(motorista)
     
     def adicionarVeiculo(self, veiculo):
-        if veiculo not in self.veiculos:
-            self.veiculos.append(veiculo)
+        if veiculo not in self._veiculos:
+            self._veiculos.append(veiculo)
         else:
             print("Veículo já cadastrado")
     def removerVeiculo(self, veiculo):
-        self.veiculos.remove(veiculo)
+        self._veiculos.remove(veiculo)
     
