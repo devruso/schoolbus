ef7ada2fd2792cce9665a863c4724e091fa51d22
devruso <jamilsonjr@live.com>
2023-12-05 19:55:26 -0300

    fix: Corrige atributos

--- fornecedor.py
+++ fornecedor.py
@@ -1,13 +1,14 @@
+from pessoaJuridica import PessoaJuridica
 class Fornecedor(PessoaJuridica):
-    def __init__(self,nome_oficial,cpf_cnpj,endereco,telefone,nome_fantasia,num_funcionarios, contratos):
+    def __init__(self,nome_oficial,cpf_cnpj,endereco,telefone,nome_fantasia,num_funcionarios):
         super().__init__(nome_oficial,cpf_cnpj,endereco,telefone,nome_fantasia,num_funcionarios)
-        self.contratos = contratos
+        self._contratos = []
     
     def exibirDados(self):
-        return f"Nome Oficial: {self.nome_oficial}\n CNPJ: {self.cpf_cnpj}\n Endereço: {self.endereco}\n Telefone: {self.telefone}\n Nome Fantasia: {self.nome_fantasia}\n Número de Funcionários: {self.num_funcionarios}\n Contratos: {self.contratos}\n"
+        return super().exibirDados() + f"\nContratos: {self._contratos}\n"
     
     def verificarTipo(self):
         return "Fornecedor"
 
     def adicionarContrato(self, contrato):
-        self.contratos.append(contrato)
+        self._contratos.append(contrato)
